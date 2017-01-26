0e5a849227374329f4610665253458d3bb2dadea
BigGluk <newbiggluk@gmail.com>
2017-01-26 16:55:46 +1000

    menu

---
+++ Assets/Resources/scripts/Menu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+    public void StartButton()
+    {
+        Application.LoadLevel(1);
+    }
+    public void SkinsButton()
+    {
+
+    }
+}
